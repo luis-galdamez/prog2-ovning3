70d06fffa6f7f0ab2864e60165dc7794ba16ed00
luisfergldmz <luisfergldmz@gmail.com>
2026-05-04 11:33:07 +0200

    Data imported and importRecordings metod implemented

--- src/main/java/se/su/ovning3/Exercise3.java
+++ src/main/java/se/su/ovning3/Exercise3.java
@@ -1,10 +1,15 @@
 package se.su.ovning3;
 
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Exercise3 {
 
@@ -12,10 +17,35 @@ public class Exercise3 {
 
 	public void exportRecordings(String fileName) {
 
+
+
 	}
 
 	public void importRecordings(String fileName) {
+		File file = new File(fileName);
+		try{
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
 
+			String line;
+			bufferedReader.readLine();
+			while((line = bufferedReader.readLine()) != null){
+				String[] parts = line.split(";");
+				int year = Integer.parseInt(parts[2]);
+				String numberOfGenresStr = bufferedReader.readLine();
+				int numberOfGenres = Integer.parseInt(numberOfGenresStr);
+				Set<String> genres = new HashSet<>();
+				for(int i = 0; i < numberOfGenres; i++){
+					genres.add(bufferedReader.readLine());
+				}
+				Recording r = new Recording(parts[0], parts[1], year, genres);
+				recordings.add(r);
+			}
+			bufferedReader.close();
+		}
+		catch(IOException e){
+			System.out.println(fileName + "File not found");
+		}
 	}
 
 	public Map<Integer, Double> importSales(String fileName) {
